e20c5d1fe5350e071d21b769ed393325d8af6063
Karina <k.vilegzhanina@gmail.com>
2015-12-02 21:01:30 +0300

    Теперь не падает

--- sources/Subdirproject/cppapp/main.cpp
+++ sources/Subdirproject/cppapp/main.cpp
@@ -10,12 +10,12 @@ int main()
     cin >> n;
     cout << "Число столбцов: ";
     cin >> m;
-    Matrix arr(n, m), arr2(n, m);
+    Matrix arr(n, m);
     for (i = 0; i < n; i++)
         for (j = 0; j < m; j++)
             arr.set(i, j, i * j);
     arr.print();
-    arr2 = arr.copy();
+    Matrix arr2 = arr.copy();
     arr2.print();
     return 0;
 }
